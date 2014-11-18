93dac073c5975b05866fc5d73c4bc08815bbcb0f
racelisd <racelisd@gmail.com>
2014-11-17 23:22:11 -0800

    final
    everything got deleted, so this is the redo

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,47 @@
-/*
-    Signup Form Script
-    This script will load the state select list and validate the form before submission
-*/
+
+document.addEventListener('DOMContentLoaded', america);
+
+function america() {
+
+		/*populates the state list */
+		for (i = 0; i < usStates.length; i++) {
+			var stateList = document.getElementsByName("state");
+			var state = document.createElement("option");
+			state.text = usStates[i].name;
+			stateList[0].appendChild(state);
+		}
+
+		/* For exiting the page */
+		var leavePage = document.getElementById("cancelButton");
+		leavePage.addEventListener('click', exit);
+
+		function exit() {
+			var exit = window.confirm("Are you sure you want to leave this sexy page and go to google instead?");
+			if (exit) {
+				window.location.replace("https://www.google.com");
+			}
+		}
+
+		/* For selecting "other" as an occupation */
+		occupation.addEventListener('change', other);
+
+		function other() { 
+	        var newChoice = document.getElementsByName("occupationOther");
+			var input = document.getElementById("occupation").value;
+			if(input === "other") {
+				newChoice[0].style.display = "inherit";
+			} else {
+	            newChoice[0].style.display = "none";
+	        }
+	    } 
+
+	    function checkBirthday(date) {
+
+
+	    }
+
+	    function checkZip(zip) {
+
+	    	
+	    }
+}
